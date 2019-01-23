a7df49da90acd4e3ae71da8b706c6b9a6039abca
MisabelMc <45104060+MisabelMc@users.noreply.github.com>
2019-01-23 14:12:11 -0300

    commit inicial

---
+++ GComun/src/main/java/com/GComun/inicio/GComunApplication.java
@@ -0,0 +1,14 @@
+package com.GComun.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GComunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GComunApplication.class, args);
+	}
+
+}
+
---
+++ GComun/src/test/java/com/GComun/inicio/GComunApplicationTests.java
@@ -0,0 +1,17 @@
+package com.GComun.inicio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GComunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
